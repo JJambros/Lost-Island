d6040fe458e1e153ed68f7693a71a32c363e8709
JJambros <143129381+JJambros@users.noreply.github.com>
2023-12-02 00:30:40 -0500

    It's Perfect

--- Lost Island/Assets/RockDrop.cs
+++ Lost Island/Assets/RockDrop.cs
@@ -4,12 +4,20 @@ using UnityEngine;
 
 public class RockDrop : MonoBehaviour
 {
-    
+    public Transform spawnPoint;
+    public Transform spawnPoint2;
+    public GameObject confetti;
+    public GameObject areaEffect;
+    public GameObject sparkle;
+
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Rock")) 
         {
-            
+            Instantiate(confetti, spawnPoint.position, spawnPoint.rotation);
+            Instantiate(areaEffect, spawnPoint2.position, spawnPoint2.rotation);
+            Instantiate(sparkle, spawnPoint.position, spawnPoint.rotation);
         }
     }
 }
+
